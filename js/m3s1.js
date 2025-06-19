2f15bbdb47c0ffea68c248f7b319431e34e09c5e
Cesar Hernandez <cehergra1999@gmail.com>
2025-06-19 11:05:23 -0500

    Add files via upload

--- js/m3s1.js
+++ js/m3s1.js
@@ -2,7 +2,7 @@ function agregarClientes(){
 
     let lisClientes=[];
 
-    for(let i=0; i<2; i++){
+    for(let i=0; i<1; i++){
 
     let id=prompt("Ingresa el id del cliente");
     let nombre=prompt("Ingresa el nombre del cliente");
@@ -12,36 +12,34 @@ function agregarClientes(){
     cliente.id=id;
     cliente.nombre=nombre;
     cliente.edad=edad;
-    cliente.estado=true
+    cliente.estado=true;
 
     let ListRole=[];
 
     if(Number(edad)<18)
         return;
     
-    for(let i=0;i<2;i++){
+    for(let i=0;i<1;i++){
         let role={};
         let id=prompt("Ingresa el id del rol");
         let nombre=prompt("Ingresa el nombre del rol");
 
         role.id=id;
         role.nombre=nombre;
-
-        ListRole.push(role);
     }
 
     cliente.roles=ListRole;
-    lisClientes.push(role); 
+    lisClientes.push(cliente); 
     }
 
     const lista = document.getElementById("cliente-lista");
 
         lisClientes.forEach(cliente =>{
             const listitem = document.createElement("li");
-            listitem.textContent = `${cliente.nombre} - ${cliente.edad} - ${cliente.estado}`;
+            listitem.textContent = `${cliente.nombre} - ${cliente.edad} - ${cliente.estado} - ${role.nombre}`;
             lista.appendChild(listitem);
         });
 
-console.log(cliente);
-alert(JSON.stringify(cliente));
+console.log(lisClientes);
+// alert(JSON.stringify(cliente));
 }
--- js/sistema_interactivo.js
+++ js/sistema_interactivo.js
@@ -1,19 +1,20 @@
-//Inicializacion del programa
-console.log("¡Bienvenido al Sistema Interactivo de Mensajes!");
+function agregarClientes(){
+    //Inicializacion del programa
+    console.log("¡Bienvenido al Sistema Interactivo de Mensajes!");
 
-//Captura de datos del programa
-let nombre = prompt("Por favor, Ingresa tu nombre:");
-let edad = prompt("Por favor, Ingresa tu edad:");
+    //Captura de datos del programa
+    let nombre = prompt("Por favor, Ingresa tu nombre:");
+    let edad = prompt("Por favor, Ingresa tu edad:");
 
-//Convertir edad a numero
-edad = parseInt(edad);
+    //Convertir edad a numero
+    edad = parseInt(edad);
 
-//Codigo para Validacion y Mensajes:
-if (isNaN(edad)){
-    console.error("Error: Por favor, ingresa una edad valida en numeros");
-} else if (edad < 18){
-    alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo!`);
-} else {
-    alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);
+    //Codigo para Validacion y Mensajes:
+    if (isNaN(edad)){ // NAN -> not a number
+        console.error("Error: Por favor, ingresa una edad valida en numeros");
+    } else if (edad < 18){
+        alert(`Hola ${nombre}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo!`);
+    } else {
+        alert(`Hola ${nombre}, eres mayor de edad. ¡Preparate para grandes oportunidades en el mundo de la programacion!`);
+    }
 }
-
